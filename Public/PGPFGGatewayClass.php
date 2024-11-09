d11dfda86ffff610dc97f400e92e241f8cb4e3db
emanuellopess <lopes.emanuel@linknacional.com>
2024-11-09 15:12:33 +0000

    chore: Update script functions with version constant

--- Public/PGPFGGatewayClass.php
+++ Public/PGPFGGatewayClass.php
@@ -50,8 +50,8 @@ final class PGPFGGatewayClass extends PaymentGateway {
      * @inheritDoc
      */
     public function getLegacyFormFieldMarkup(int $formId, array $args): string {
-        wp_enqueue_style('pgpfg-public', PGPFG_PIX_PLUGIN_URL . 'Public/css/pgpfg-public.css');
-        wp_enqueue_style('pgpfg-material-symbols-outlined', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0');
+        wp_enqueue_style('pgpfg-public', PGPFG_PIX_PLUGIN_URL . 'Public/css/pgpfg-public.css', array(), PGPFG_PIX_PLUGIN_VERSION);
+        wp_enqueue_style('pgpfg-material-symbols-outlined', 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0', array(), PGPFG_PIX_PLUGIN_VERSION);
 
         // Array de argumentos
         $template_args = array(
